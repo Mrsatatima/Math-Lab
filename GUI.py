18d48e1c7dd0c6521f0cd5e3e6e5131509020213
Al-amin Ahmad Abbas <aminuozil@gmail.com>
2021-02-03 22:54:44 +0100

    Update GUI.py and formulas
    Add formulas to calculate Volume and Surface area

--- GUI.py
+++ GUI.py
@@ -1,10 +1,10 @@
 #User interface
-from formulas import Circle, Triangle, Square
+from formulas import Geometry
 
 def main():
     running = True
     while running:
-        cal = (input("Enter C for Cirle.... Enter T for Triangle..... Enter S for Square.... Enter Q to Quit\n Enter: " )).upper()
+        cal = (input("Enter C for Cirle.... Enter A for Triangle..... Enter S for Square.... Enter Q to Quit\\\nEnter T  for Trapezium.... Enter P for Parallelogram.... \nEnter: " )).upper()
         if cal == "Q":
             running = False
         elif cal == "C":
@@ -13,7 +13,7 @@ def main():
             except:
                 print("############\ninvalid Entry.... Insert Numbers Only\n###########")
                 continue
-            circle = Circle(r)
+            circle = Geometry.Circle(r)
             geometry = (input("Enter A for Area.... Enter C for Circumference......  Enter D for Diameter \n Enter: ")).upper()
             if geometry == "A":
                 print("############\n",circle.area(),"\n###########")
@@ -21,7 +21,10 @@ def main():
                 print("############\n",circle.circumference(),"\n###########")
             elif geometry == "D":
                 print("############\n",circle.diameter(),"\n###########")
-        elif cal == "T":
+            else:
+                print("############\ninvalid entry\n###########")
+
+        elif cal == "A":
             geometry = (input("Enter A for Area.... Enter P for Perimeter\n Enter: ")).upper()
             if geometry == "A":
                 try:
@@ -30,7 +33,7 @@ def main():
                 except:
                     print("############\ninvalid Entry.... Insert Numbers Only\n###########")
                     continue
-                triangle = Triangle(height, base)
+                triangle = Geometry.Triangle(height, base)
                 print("############\n",triangle.area(),"\n###########")
             elif geometry == "P":
                 try:
@@ -40,8 +43,77 @@ def main():
                 except:
                     print("############\ninvalid Entry.... Insert Numbers Only\n###########")
                     continue
-                triangle = Triangle(height, base, side)
+                triangle = Geometry.Triangle(height, base, side)
                 print("############\n",triangle.perimeter(),"\n###########")
+            else:
+                print("############\ninvalid entry\n###########")
+
+        elif cal == "S":
+            try:
+                lenght = float(input("Enter Lenght: "))
+                width = float(input("Enter Width: "))
+            except:
+                print("############\ninvalid Entry.... Insert Numbers Only\n###########")
+                continue
+            square = Geometry.Square(lenght, width)
+            geometry = (input("Enter A for Area.... Enter P for Perimeter\n Enter: ")).upper()
+            if geometry == "A":
+                print("############\n",square.perimeter(),"\n###########")
+            elif geometry == "P":
+                print("############\n",square.perimeter(),"\n###########")
+
+            else:
+                print("############\ninvalid entry\n###########")
+
+        elif cal == "T":
+            geometry = (input("Enter A for Area.... Enter P for Perimeter\n Enter: ")).upper()
+            if geometry == "A":
+                try:
+                    baseA = float(input("Enter Base A: "))
+                    baseB = float(input("Enter Base B: "))
+                    height = float(input("Enter Height: "))
+                except:
+                    print("############\ninvalid Entry.... Insert Numbers Only\n###########")
+                    continue
+                trapezium = Geometry.Trapezium(baseA, baseB, height)
+                print("############\n",trapezium.area(),"\n###########")
+            elif geometry == "P":
+                try:
+                    baseA = float(input("Enter Base A: "))
+                    baseB = float(input("Enter Base B: "))
+                    sideA = float(input("Enter Side A: "))
+                    sideB = float(input("Enter Side B: "))
+                except:
+                    print("############\ninvalid Entry.... Insert Numbers Only\n###########")
+                    continue
+                trapezium = Geometry.Trapezium(baseA, baseB, sideA, sideB)
+                print("############\n",trapezium.perimeter(),"\n###########")
+            else:
+                print("############\ninvalid entry\n###########")
+
+        elif cal == "P":
+            geometry = (input("Enter A for Area.... Enter P for Perimeter\n Enter: ")).upper()
+            if geometry == "A":
+                try:
+                    base = float(input("Enter Base: "))
+                    height = float(input("Enter Height: "))
+                except:
+                    print("############\ninvalid Entry.... Insert Numbers Only\n###########")
+                    continue
+                parallelogram = Geometry.Parallelogram(base, height)
+                print("############\n",parallelogram.area(),"\n###########")
+            elif geometry == "P":
+                try:
+                    base = float(input("Enter Base: "))
+                    side = float(input("Enter Side: "))
+                except:
+                    print("############\ninvalid Entry.... Insert Numbers Only\n###########")
+                    continue
+                parallelogram = Geometry.Parallelogram(base, height)
+                print("############\n",parallelogram.perimeter(),"\n###########")
+            else:
+                print("############\ninvalid entry\n###########")
+
         else:
             print("############\ninvalid entry\n###########")
 if __name__ == "__main__":
--- formulas.py
+++ formulas.py
@@ -1,58 +1,149 @@
 #funcrions for the all calculations
 from Constants import PI
-class Circle:
-    def __init__ (self, radius):
-        self.radius = radius
+class Geometry:
+        class Circle:
+            def __init__ (self, radius):
+                self.radius = radius
 
-    def area(self):
-        """
-            calculates the area of a circle
-            output --> returns a float
-        """
-        return PI*(self.radius**2)
-    def circumference(self):
-        """
-            calculates the area of a circle
-            output --> returns a float
-        """
-        return 2 *(PI*self.radius)
-    def diameter(self):
-        """
-            calculates the area of a circle
-            output --> returns a float
-        """
-        return 2 * self.radius
+            def area(self):
+                """
+                    calculates the area of a circle
+                    output --> returns a float
+                """
+                return PI*(self.radius**2)
+            def circumference(self):
+                """
+                    calculates the area of a circle
+                    output --> returns a float
+                """
+                return 2 *(PI*self.radius)
+            def diameter(self):
+                """
+                    calculates the area of a circle
+                    output --> returns a float
+                """
+                return 2 * self.radius
 
+        class Triangle:
+            def __init__(self, height, base, side=None):
+                self.height = height
+                self.base = base
+                self.side = side
+            def area(self):
+                """
+                    calculates the area of a Triangle
+                    output --> returns a float
+                """
+                return (self.height * self.base)/2
+            def perimeter(self):
+                """
+                    calculates the area of a Triangle
+                    output --> returns a float
+                """
+                return self.height + self.base + self.side
 
-class Triangle:
-    def __init__(self, height, base, side=None):
-        self.height = height
-        self.base = base
-        self.side = side
-    def area(self):
-        """
-            calculates the area of a Triangle
-            output --> returns a float
-        """
-        return (self.height * self.base)/2
-    def perimeter(self):
-        """
-            calculates the area of a Triangle
-            output --> returns a float
-        """
-        return self.height + self.base + self.side
+        class Square:
 
-class Square:
-    def __init__(self, lenght, width):
-        self.lenght = lenght
-        self.width = width
-    def area(self):
-        if self.lenght == self.width:
-            return self.lenght**2
-        else:
-            return self.lenght * self.width
-    def perimeter(self):
-        if self.lenght == self.width:
-            return 4 * self.lenght
-        else:
-            return 2*(self.lenght + self.width)
+            def __init__(self, lenght, width):
+                self.lenght = lenght
+                self.width = width
+            def area(self):
+                """
+                    calculates the area of a Square
+                    output --> returns a float
+                """
+                if self.lenght == self.width:
+                    return self.lenght**2
+                else:
+                    return self.lenght * self.width
+            def perimeter(self):
+                """
+                    calculates the perimeter of a Square
+                    output --> returns a float
+                """
+                if self.lenght == self.width:
+                    return 4 * self.lenght
+                else:
+                    return 2*(self.lenght + self.width)
+
+        class Trapezium:
+            def __init__(self, baseA, baseB, height=1, side=None):
+                self.height = height
+                self.baseA = baseA
+                self.baseB = baseB
+                self.sideA = height
+                self.sideB = side
+            def area(self):
+                """
+                    calculates the area of a Trapezium
+                    output --> returns a float
+                """
+                return self.height * (self.baseA + self.baseB)/2
+            def perimeter(self):
+                """
+                    calculates the perimeter of a Trapezium
+                    output --> returns a float
+                """
+                return float(self.baseA + self.baseB + self.sideA + self.sideB)
+
+        class Parallelogram:
+            def __init__(self, base, height,):
+                self.base = base
+                self.height = height
+                self.side = height
+            def area(self):
+                """
+                    calculates the area of a Parallelogram
+                    output --> returns a float
+                """
+                return float(self.base * self.height)
+
+            def perimeter(self):
+                """
+                    calculates the perimeter of a Parallelogram
+                    output --> returns a float
+                """
+                return 2 *(self.side + self.base)
+
+        class Cube:
+            def __init__(self, lenght, width, height):
+                self.lenght = lenght
+                self.width = width
+                self.height = height
+            def volume (self):
+                if self.width == self.height == self.lenght:
+                    return self.width**3
+                else:
+                    return self.width * self.height * self.lenght
+            def surface_area(self):
+                if self.width == self.height == self.lenght:
+                    return 6 *self.width**2
+                else:
+                    return 2*((self.width * self.height) + (self.height* self.lenght) + (self.width* self.lenght))
+
+        class Sphere:
+            def init(self, radius):
+                self.radius = radius
+            def volume(self):
+                return (4/3) * PI * radius ** 3
+            def surface_area(self):
+                return 4 * PI * radius **2
+
+        class Cylinder:
+            def __init__(self, radius, height):
+                self.radius = radius
+                self.height = height
+            def volume(self):
+                return PI * (self.radius**2) * self.height
+            def surface_area(self):
+                return 	2(PI * self.radius**2) + 2 * PI * self.radius * self.height
+
+        class Cone:
+            def __init__(self, radius, height):
+                self.radius = radius
+                self.height = height
+                self.side = height
+            def volume(self):
+                return (1/3) * PI * self.radius * self.height
+            def surface_area(self):
+                return PI * radius * self.side
